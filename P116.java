371fe47cb178b97c61758ffb140ef96f082f82ee
AdNauseam24 <pascualquiles13@gmail.com>
2024-10-24 20:18:41 +0200

    ¡Hola mundo!

--- P116.java
+++ P116.java
@@ -1,27 +1,23 @@
 package condicionales;
 import java.util.Scanner;
-public class HolaMundo {
+
+//accepted
+
+public class P116 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Introduzca un numero entre 0 y 5: ");
         int n =sc.nextInt();
         switch (n){
             case 5:
-                System.out.println("Hola Mundo");
+                System.out.println("Hola mundo.");
             case 4:
-                System.out.println("Hola Mundo");
+                System.out.println("Hola mundo.");
             case 3:
-                System.out.println("Hola Mundo");
+                System.out.println("Hola mundo.");
             case 2:
-                System.out.println("Hola Mundo");
+                System.out.println("Hola mundo.");
             case 1:
-                System.out.println("Hola Mundo");
-                break;
-            case 0:
-                System.out.println("Se introdujo un 0");
-                break;
-            default:
-                System.out.println("Número inválido");
+                System.out.println("Hola mundo.");
                 break;
         }
     }
