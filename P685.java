9761f338710c612b51162320b5d91d12419e6e7f
AdNauseam24 <pascualquiles13@gmail.com>
2024-10-25 20:33:09 +0200

    Espantaperros

--- P685.java
+++ P685.java
@@ -1,75 +1,26 @@
-package buclesSimples;
-
+package condicionales;
 import java.util.Scanner;
 
-public class Espantaperros {
+
+//accepted
+
+
+public class P685 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese las cordenadas x y x y: ");
-        String coordenadas = sc.nextLine();
-        //asumimos que en principio no se detecta y creamos una variable para cada una de las coordenadas
-        boolean detectado = false;
-        int xTorre = Integer.parseInt(coordenadas.substring(0,1));
-        int yTorre = Integer.parseInt(coordenadas.substring(2,3));
-        int xIntruso = Integer.parseInt(coordenadas.substring(4,5));
-        int yIntruso = Integer.parseInt(coordenadas.substring(6,7));
-        //hasta que el usuario introduzca manualmente todo 0 se repetira el programa
-        while (xTorre!=0 || yTorre!=0 || xIntruso!=0 || yIntruso != 0) {
-            //si uno de los ejes coincide significa que está en su campo de vision
-            if (xTorre==xIntruso || yTorre==yIntruso) {
-                detectado = true;
-                //en cualquier otro caso se nos queda la cuadricula dividida en 4 espacios
-                //segun sea la posicion relativa de los dos objetos se comprobará en una direccion u otra
-            } else {
-                //en el primer caso se escanea aumentando x e y
-                //si alguno de los dos numeros sobrepasa el de las coordenadas del intruso significa que ya no lo detecta
-                //si las dos coordenadas se vuelven coincidentes se ha detectado
-                if (xIntruso>xTorre && yIntruso>yTorre) {
-                    while (xIntruso>xTorre && yIntruso>yTorre){
-                        xTorre++;
-                        yTorre++;
-                        if (xTorre==xIntruso && yTorre==yIntruso) {
-                            detectado = true;
-                        }
-                    }
-                } else if (xIntruso>xTorre && yIntruso<yTorre) {
-                    while (xIntruso>xTorre && yIntruso<yTorre){
-                        xTorre++;
-                        yTorre--;
-                        if (xTorre==xIntruso && yTorre==yIntruso) {
-                            detectado = true;
-                        }
-                    }
-                } else if (xIntruso<xTorre && yIntruso<yTorre) {
-                    while (xIntruso<xTorre && yIntruso<yTorre){
-                        xTorre--;
-                        yTorre--;
-                        if (xTorre==xIntruso && yTorre==yIntruso) {
-                            detectado = true;
-                        }
-                    }
-                } else if (xIntruso<xTorre && yIntruso>yTorre) {
-                        while (xIntruso<xTorre && yIntruso>yTorre){
-                            xTorre--;
-                            yTorre++;
-                            if (xTorre==xIntruso && yTorre==yIntruso) {
-                                detectado = true;
-                            }
-                        }
-                }
-            }
-            if (detectado) {
+        while (true){
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+            if (x1==0 && y1==0 && x2==0 && y2==0){
+                break;
+            } else if (x1==x2 || y1==y2 || Math.abs(x1-x2)==Math.abs(y1-y2)) {
                 System.out.println("SI");
-            }else {
+            } else {
                 System.out.println("NO");
             }
-            System.out.println("Ingrese las cordenadas x y x y: ");
-             coordenadas = sc.nextLine();
-             detectado = false;
-             xTorre = Integer.parseInt(coordenadas.substring(0,1));
-             yTorre = Integer.parseInt(coordenadas.substring(2,3));
-             xIntruso = Integer.parseInt(coordenadas.substring(4,5));
-             yIntruso = Integer.parseInt(coordenadas.substring(6,7));
+
         }
     }
 }
