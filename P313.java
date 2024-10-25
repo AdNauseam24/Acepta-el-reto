7206628e12bd893d90d2db85773d1ec3de0a5cd4
AdNauseam24 <pascualquiles13@gmail.com>
2024-10-25 10:07:12 +0200

    Fin de mes

--- P313.java
+++ P313.java
@@ -1,23 +1,20 @@
 package condicionales;
 import java.util.Scanner;
-public class FinMes {
+
+
+//accepted
+
+
+public class P313 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int saldo, cambio, resultado;
-        System.out.println("Introduzca el número de casos de prueba");
-        int casos = sc.nextInt();
-        //Hacemos un bucle que se repite el numero de veces que indica el usuario
-        for (int i = 0; i<casos; i++) {
-            System.out.println("Introduzca el saldo de su cuenta: ");
-            saldo = sc.nextInt();
-            System.out.println("Introduzca el cambio de balance: ");
-            cambio = sc.nextInt();
-            //calculamos en cuento se queda el saldo de la cuenta
-            resultado = saldo + cambio;
-            //Si el resultado es 0 o mayor llega a fin de mes
-            if(resultado>=0){
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int primerDia = sc.nextInt();
+            int cambio = sc.nextInt();
+            if (primerDia+cambio>=0){
                 System.out.println("SI");
-            }else {
+            } else {
                 System.out.println("NO");
             }
         }
