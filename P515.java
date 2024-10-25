b8040abbcd1e21ebdd6c29efbcf093678d3f3637
AdNauseam24 <pascualquiles13@gmail.com>
2024-10-25 20:15:40 +0200

    Poniendo la mesa

--- P515.java
+++ P515.java
@@ -1,32 +1,31 @@
 package buclesSimples;
-
 import java.util.Scanner;
 
-public class PoniendoMesa {
+
+//accepted
+
+
+
+public class P515 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Introduzca el número de copas que hay que llevar: ");
-        int nCopas = sc.nextInt();
-        //El programa continua hasta que el usuario introduce manualmente un 0
-        while (nCopas!=0) {
-            int nPaseo=1;
-            int contadorPaseos=0;
-            //el subproceso de contar cuantos viajes se hacen se realiza mientras haya mas de 0 copas
-            while (nCopas > 0) {
-                //si el numero es impar se resta una copa, si es par 2
-                if (nPaseo % 2 != 0) {
-                    nCopas--;
-                } else {
-                    nCopas -= 2;
-                }
-                //tras cada viaje se suma 1 al contador y 1 al numero de viaje, que comienza desde 1
-                contadorPaseos++;
-                nPaseo++;
+        int copas = sc.nextInt();
+        while (copas!=0){
+            int paseo=0;
+            while (copas>0){
+                paseo++;
+            if (paseo%2!=0){
+                copas--;
+            } else if (copas%2==0){
+                copas-=2;
+            } else {
+                copas--;
             }
-            //se muestra cuantos viajes han tenido que hacerse y se vuelve a pedir al usuario qeu indique una cantidad de copas
-            System.out.println(contadorPaseos);
-            System.out.println("Introduzca el número de copas que hay que llevar: ");
-             nCopas = sc.nextInt();
+
+            }
+            System.out.println(paseo);
+            copas=sc.nextInt();
+
         }
     }
 }
