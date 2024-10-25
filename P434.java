8ce9302847c5573f3d7e9632cae5d17b386286e9
AdNauseam24 <pascualquiles13@gmail.com>
2024-10-25 19:40:46 +0200

    Romance en el palomar

--- P434.java
+++ P434.java
@@ -1,25 +1,20 @@
-package buclesSimples;
-
+package condicionales;
 import java.util.Scanner;
 
-public class Palomar {
+
+//accepted
+
+
+public class P434 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Introduzca el número de casos de prueba");
-        int num = sc.nextInt();
-        int n, m;
-        for (int i = 0; i < num; i++) {
-            System.out.println("Introduzca el número de palomas");
-            n = sc.nextInt();
-            while (n<2){
-                System.out.println("El número de palomas debe ser al menos 2");
-                n = sc.nextInt();
-            }
-            System.out.println("Introduzca el numero de palomares: ");
-            m = sc.nextInt();
-            if(n>m){
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            int palomas = sc.nextInt();
+            int cajas = sc.nextInt();
+            if (palomas>cajas){
                 System.out.println("PRINCIPIO");
-            }else {
+            } else {
                 System.out.println("ROMANCE");
             }
         }
