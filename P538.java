4e97b6b3a5bab2c7b8fc74ab24d9b0028cda52c4
AdNauseam24 <pascualquiles13@gmail.com>
2024-10-25 20:22:27 +0200

    Toesto era campo

--- P538.java
+++ P538.java
@@ -1,30 +1,23 @@
-package buclesSimples;
-
+package condicionales;
 import java.util.Scanner;
 
-public class ToestoCampo {
+
+//accepted
+
+
+public class P538 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese los años que tenia el abuelo y los año de la iglesia: ");
-        String anios = sc.nextLine();
-        //separamos los años del abuelo y los de la iglesia
-        String [] parts = anios.split(" ");
-        int anioAbuelo = Integer.parseInt(parts[0]);
-        int anioIglesia = Integer.parseInt(parts[1]);
-        //mientras al menos uno de los dos no sea 0 se ejecutará el bucle
-        while (anioAbuelo!=0 || anioIglesia!=0) {
-            //si el abuelo tiene mas años o los mismos que la iglesia esta cuerdo
-            if (anioAbuelo>=anioIglesia) {
-                System.out.println("CUERDO");
-            } else {
+        int abuelo = sc.nextInt();
+        int iglesia = sc.nextInt();
+        while (abuelo!=0 || iglesia!=0) {
+            if (iglesia>abuelo){
                 System.out.println("SENIL");
+            } else {
+                System.out.println("CUERDO");
             }
-            //volvemos a pedir los datos para saber si se sigue ejecutando el bucle
-            System.out.println("Ingrese los años que tenia el abuelo y los año de la iglesia: ");
-             anios = sc.nextLine();
-             parts = anios.split(" ");
-             anioAbuelo = Integer.parseInt(parts[0]);
-             anioIglesia = Integer.parseInt(parts[1]);
+            abuelo = sc.nextInt();
+            iglesia = sc.nextInt();
         }
     }
 }
